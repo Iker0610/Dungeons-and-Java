ad0c0db6609e54e8c58d3e2226e79c1c60ebcf7b
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2019-03-11 23:44:00 +0100

    Update

--- src/juego/componentes/dungeon/objetos/recogibles/ObjetoClave.java
+++ src/juego/componentes/dungeon/objetos/recogibles/ObjetoClave.java
@@ -2,9 +2,10 @@ package juego.componentes.dungeon.objetos.recogibles;
 
 public class ObjetoClave extends ObjetoRecogible {
     //Atributos
-
+    private String descripcion;
     //Constructor
-    public ObjetoClave (String pNombre){
+    public ObjetoClave (String pNombre, String pDescripcion){
         super(pNombre);
+        this.descripcion = pDescripcion;
     }
 }
