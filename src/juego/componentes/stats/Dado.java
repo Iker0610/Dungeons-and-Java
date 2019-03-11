e8124088e1635bb44430960569d427ffe6fa62cc
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2019-03-11 19:28:31 +0100

    Creacion del dado

---
+++ src/juego/componentes/stats/Dado.java
@@ -0,0 +1,16 @@
+package juego.componentes.stats;
+
+public class Dado {
+
+    //Atributos
+
+    //Constructora
+    public Dado(){//TODO}
+
+    //Otros metodos
+    public int lanzarDado(){
+        int valorDado;
+        //TODO
+        return valorDado;
+    }
+}
