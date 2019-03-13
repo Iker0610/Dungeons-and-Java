5519829fb67f528f2eaf1e6f4b02c1eb7940cd0e
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2019-03-14 00:04:49 +0100

    Update

--- src/juego/componentes/estancias/objetos/JefeFinal.java
+++ src/juego/componentes/estancias/objetos/JefeFinal.java
@@ -1,4 +1,51 @@
 package juego.componentes.estancias.objetos;
 
+import juego.componentes.herramientas.ListaStat;
+
 public class JefeFinal {
+    //Atributos
+    private static JefeFinal jefeFinal;
+    private String nombre;
+    private ListaStat lStats;
+    private int vidaActual;
+    //Frases de interaccion
+    private String fraseBienvenida;
+    private String fraseDerrota;
+    private String fraseVictoria;
+
+    //Constructora
+    private JefeFinal (){
+        //TODO
+    }
+
+    //Patron Singleton
+    public static JefeFinal getJefeFinal(){
+        if (jefeFinal == null){
+            jefeFinal = new JefeFinal();
+        }
+        return jefeFinal;
+    }
+
+    //Metodos de comunicacion
+    public void darVienvenida(){
+        //TODO
+    }
+
+    public void imprimirFraseDerrota(){
+        //TODO
+    }
+
+    public void imprimirFraseVictoria(){
+        //TODO
+    }
+
+    //Metodos de lucha contra el jefe
+    private void derrotarJefe(){
+        //TODO
+    }
+
+    public void atacarJefe(int pDano){
+        //TODO
+    }
+
 }
