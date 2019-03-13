c572408a73354dcb95535956103c70810ef71307
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2019-03-13 22:09:19 +0100

    Update

--- src/juego/componentes/estancias/Estancia.java
+++ src/juego/componentes/estancias/Estancia.java
@@ -1,20 +1,14 @@
 package juego.componentes.estancias;
 
-import juego.componentes.estancias.objetos.interactivos.Cofre;
-import juego.componentes.estancias.objetos.interactivos.Npc;
-import juego.componentes.estancias.objetos.interactivos.Puerta;
-
-import java.util.ArrayList;
-
 public class Estancia {
 
     //Atributos
     private String idEstancia;
 
     //Listado de objetos interactivos
-    private ArrayList<Puerta> listaPuertas;
-    private ArrayList<Cofre> listaCofres;
-    private ArrayList<Npc> listaNpc;
+    private ListaPuertas listaPuertas;
+    private ListaCofres listaCofres;
+    private ListaNPCs listaNpc;
 
     //Constructora
     public Estancia (String pIdEstancia){
