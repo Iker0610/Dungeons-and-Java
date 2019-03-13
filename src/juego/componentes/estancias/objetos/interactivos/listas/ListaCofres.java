c5b2838cd67e0f419943c57d592288a981ebc78a
AdrianOrueta00 <48363332+AdrianOrueta00@users.noreply.github.com>
2019-03-13 21:26:30 +0100

    Add files via upload

---
+++ src/juego/componentes/estancias/objetos/interactivos/listas/ListaCofres.java
@@ -0,0 +1,26 @@
+package src.juego.dungeon.listas;
+import java.util.*;
+
+public class ListaCofres {
+	//atributos
+	private ArrayList<src.juego.componentes.dungeon.objetos.interactivos.Cofre> lista;
+	
+	//constructora
+	public ListaCofres(){
+		
+	}
+	
+	//otros metodos
+	public void annadirCofre(){
+		
+	}
+	
+	public void eliminarCofre(){
+		
+	}
+	
+	public void mostrarCofres(){
+		
+	}
+	
+}
---
+++ src/juego/componentes/estancias/objetos/interactivos/listas/ListaNPCs.java
@@ -0,0 +1,22 @@
+package src.juego.dungeon.listas;
+import java.util.*;
+
+public class ListaNPCs {
+	//atributos
+	private ArrayList<src.juego.componentes.dungeon.objetos.interactivos.Npc> lista;
+	
+	//constructora
+	public ListaNPCs(){
+		
+	}
+	
+	//otros metodos
+	public void annadirNPC(){
+		
+	}
+	
+	public void mostrarNPCs(){
+		
+	}
+	
+}
