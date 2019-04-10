191b7639c4a14a85b7de0f34c31f887785595b98
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2019-04-10 13:39:22 +0200

    Implementación LectorConsola

--- src/juego/herramientas/LectorConsola.java
+++ src/juego/herramientas/LectorConsola.java
@@ -1,13 +1,20 @@
 package juego.herramientas;
 
+import juego.herramientas.excepciones.ExcepcionFormatoIncorrecto;
+import juego.herramientas.excepciones.ExcepcionValorFueradDeRango;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class LectorConsola {
 
     //Atributos
     private static LectorConsola lectorConsola;
+    private Scanner sc;
 
     //Constructora
     private LectorConsola() {
-        //TODO
+        sc = new Scanner (System.in);
     }
 
     //Patron singleton
@@ -18,15 +25,41 @@ public class LectorConsola {
         return lectorConsola;
     }
     //Otros metodos
-    public int leerOpcion(int pValorMin, int pValorMax){
+    public int leerOpcionNum(int pValorMin, int pValorMax) throws ExcepcionFormatoIncorrecto, ExcepcionValorFueradDeRango {
+        int input;
+        try{
+            input= sc.nextInt();
+        }
+        catch(InputMismatchException e){
+            throw new ExcepcionFormatoIncorrecto();
+        }
+        if(input<pValorMin||pValorMax<input){
+            throw new ExcepcionValorFueradDeRango();
+        }
+        return input;
+    }
+
+    public String leerOpcionString() throws ExcepcionValorFueradDeRango{
         //TODO
     }
 
     public String leerString(){
-        //TODO
+        return sc.nextLine();
     }
 
-    public boolean leerBoolean(){
-        //TODO
+    public boolean leerBoolean()throws ExcepcionFormatoIncorrecto{
+        String input = sc.nextLine();
+        boolean eleccion;
+        if(input.equalsIgnoreCase("si") || input.equalsIgnoreCase("s")){
+            eleccion = true;
+        }
+        else if(input.equalsIgnoreCase("no") || input.equalsIgnoreCase("n")){
+            eleccion = false;
+        }
+        else{
+            throw new ExcepcionFormatoIncorrecto();
+        }
+
+        return eleccion;
     }
 }
