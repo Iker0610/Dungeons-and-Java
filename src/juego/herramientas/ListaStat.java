57f8511c6f64d7d2e2ebed04b8980579a4ae54a8
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2019-04-16 09:05:16 +0200

    ListaStats
    Poner límites a los stats tal que no puedan ser menor a 0 y mayores que 100

--- src/juego/herramientas/ListaStat.java
+++ src/juego/herramientas/ListaStat.java
@@ -10,6 +10,16 @@ public class ListaStat {
 	//constructora
 	public ListaStat(int pFuerza, int pDefensa, int pInteligencia, int pDestreza){
 		lStats = new HashMap<>();
+
+		if(pFuerza>100)				{pFuerza		= 100;}
+		else if (pFuerza<0)			{pFuerza		= 0;}
+		if(pDefensa>100)			{pDefensa		= 100;}
+		else if (pDefensa<0)		{pDefensa		= 0;}
+		if(pInteligencia>100)		{pInteligencia	= 100;}
+		else if (pInteligencia<0)	{pInteligencia	= 0;}
+		if(pDestreza>100)			{pDestreza		= 100;}
+		else if (pDestreza<0)		{pDestreza		= 0;}
+
 		lStats.put("Fuerza",pFuerza);
 		lStats.put("Defensa",pDefensa);
 		lStats.put("Inteligencia",pInteligencia);
@@ -19,18 +29,38 @@ public class ListaStat {
 	//otros metodos
 	public void sumarStats(ListaStat pLStats){
 		//post: los stats de la lista parametro se han sumado a la lista invocadora
-		this.lStats.put("Fuerza",this.lStats.get("Fuerza")+pLStats.getValorStat("Fuerza"));
-		this.lStats.put("Defensa",this.lStats.get("Defensa")+pLStats.getValorStat("Defensa"));
-		this.lStats.put("Inteligencia",this.lStats.get("Inteligencia")+pLStats.getValorStat("Inteligencia"));
-		this.lStats.put("Destreza",this.lStats.get("Destreza")+pLStats.getValorStat("Destreza"));
+		int fuerza = this.lStats.get("Fuerza")+pLStats.getValorStat("Fuerza");
+		int defensa = this.lStats.get("Defensa")+pLStats.getValorStat("Defensa");
+		int inteligencia = this.lStats.get("Inteligencia")+pLStats.getValorStat("Inteligencia");
+		int destreza = this.lStats.get("Destreza")+pLStats.getValorStat("Destreza");
+
+		if(fuerza>100)				{fuerza			= 100;}
+		if(defensa>100)				{defensa		= 100;}
+		if(inteligencia>100)		{inteligencia	= 100;}
+		if(destreza>100)			{destreza		= 100;}
+
+		this.lStats.put("Fuerza",fuerza);
+		this.lStats.put("Defensa",defensa);
+		this.lStats.put("Inteligencia",inteligencia);
+		this.lStats.put("Destreza",destreza);
 	}
 
 	public void restarStats(ListaStat pLStats){
 		//post: los stats de la lista parametro se han restado a la lista invocadora
-		this.lStats.put("Fuerza",this.lStats.get("Fuerza")-pLStats.getValorStat("Fuerza"));
-		this.lStats.put("Defensa",this.lStats.get("Defensa")-pLStats.getValorStat("Defensa"));
-		this.lStats.put("Inteligencia",this.lStats.get("Inteligencia")-pLStats.getValorStat("Inteligencia"));
-		this.lStats.put("Destreza",this.lStats.get("Destreza")-pLStats.getValorStat("Destreza"));
+		int fuerza = this.lStats.get("Fuerza")-pLStats.getValorStat("Fuerza");
+		int defensa = this.lStats.get("Defensa")-pLStats.getValorStat("Defensa");
+		int inteligencia = this.lStats.get("Inteligencia")-pLStats.getValorStat("Inteligencia");
+		int destreza = this.lStats.get("Destreza")-pLStats.getValorStat("Destreza");
+
+		if(fuerza<0)			{fuerza			= 0;}
+		if(defensa<0)			{defensa		= 0;}
+		if(inteligencia<0)		{inteligencia	= 0;}
+		if(destreza<0)			{destreza		= 0;}
+
+		this.lStats.put("Fuerza",fuerza);
+		this.lStats.put("Defensa",defensa);
+		this.lStats.put("Inteligencia",inteligencia);
+		this.lStats.put("Destreza",destreza);
 	}
 
 	public int getValorStat(String pStat) {
