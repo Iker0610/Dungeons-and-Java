cc0c88ef565be6d3c86cf956b9e02c1451a57768
Iker0610 <48362673+Iker0610@users.noreply.github.com>
2019-04-19 20:26:09 +0200

    Implementacion clase Dungeon
    Constructora y carga de métodos
    Aviso en el metodo buscar Estancia

--- src/juego/componentes/estancias/Dungeon.java
+++ src/juego/componentes/estancias/Dungeon.java
@@ -1,9 +1,14 @@
 package juego.componentes.estancias;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 import juego.Partida;
+import juego.herramientas.excepciones.ExcepcionEstanciaInexistente;
 
 public class Dungeon {
 
@@ -14,7 +19,48 @@ public class Dungeon {
     //Constructora
     private Dungeon(){
         lEstancias = new ArrayList<>();
-        //TODO
+        String dirRoot = System.getProperty("user.dir")+ File.separator+"dungeons"+File.separator;
+        String dirDataDungeon = dirRoot+"dungeon_info.txt";
+        try {
+            InputStream fichDataDungeon = new FileInputStream(dirDataDungeon);
+            Scanner sc = new Scanner(fichDataDungeon);
+
+            String lineaAct = sc.nextLine();
+            if (lineaAct.matches("titulo&(.*)")){
+                String[] titulo = lineaAct.split("&");
+                System.out.println(titulo[1]);
+            }
+            else{throw new Exception();}
+
+            lineaAct = sc.nextLine();
+            if (lineaAct.matches("descripcion&(.*)")){
+                String[] descripcion = lineaAct.split("&");
+                System.out.println(descripcion[1]);
+            }
+            else{throw new Exception();}
+
+            boolean estanciaFinal = false;
+            while(sc.hasNext()){
+                lineaAct = sc.nextLine();
+                if (lineaAct.matches("estanciaFinal&(.*)")){
+                    String[] estanciaFinalData = lineaAct.split("&");
+                    lEstancias.add(new EstanciaFinal(estanciaFinalData[1]));
+                    estanciaFinal=true;
+                }
+                else{lEstancias.add(new EstanciaStandar(lineaAct));}
+            }
+            if (!estanciaFinal){
+                throw new ExcepcionEstanciaInexistente();
+            }
+        }
+        catch(ExcepcionEstanciaInexistente e){
+            System.out.println("El fichero "+dirDataDungeon+" no contiene una estanciaFinal por lo que el juego no puede ejecutarse");
+            System.exit(0);
+        }
+        catch(Exception e){
+            System.out.println("El fichero "+dirDataDungeon+" no exite o no contiene el formato adecuado por lo que el juego no puede ejecutarse");
+            System.exit(0);
+        }
     }
 
     //Patron singleton
@@ -42,7 +88,8 @@ public class Dungeon {
     }
 
     //Otros metodos
-    public void cambiarEstancia(String pIdEstancia){
+    public void cambiarEstancia(String pIdEstancia) throws ExcepcionEstanciaInexistente{
+        //TODO hace falta añadir la excepción por si la estancia es NULL !!!!
     	Partida.getPartida().cambiarEstancia(this.buscarEstancia(pIdEstancia));
     }
 }
