e25d1c7c904aef8f7b1531a3f1a4e35632857930
IriaSanMiguel <48363337+IriaSanMiguel@users.noreply.github.com>
2019-04-28 20:45:29 +0200

    Update CondicionStatTest.java

--- test/CondicionStatTest.java
+++ test/CondicionStatTest.java
@@ -1,22 +1,34 @@
 import static org.junit.Assert.*;
 
+import juego.componentes.estancias.objetos.interactivos.condiciones.numericas.*;
+import juego.componentes.jugador.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class CondicionStatTest {
+	CondicionStat c1;
+	CondicionStat c2;
+	Jugador j1;
 
 	@Before
 	public void setUp() throws Exception {
+		c1=new CondicionStat(0,"Destreza");
+		c2=new CondicionStat(100,"Destreza");
+		j1=new Jugador("Laura","m","Elfo","Mago");
 	}
 
 	@After
 	public void tearDown() throws Exception {
+		c1=null;
+		c2=null;
+		j1=null;
 	}
 
 	@Test
 	public void testCumpleLaCondicion() {
-		fail("Not yet implemented");
+		assertTrue(c1.cumpleLaCondicion(j1));
+		assertFalse(c2.cumpleLaCondicion(j1));
 	}
 
 }
